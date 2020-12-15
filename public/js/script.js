9f36bb5359973ff7e90017c3cc08e7d474963f00
amandatell <51403678+amandatell@users.noreply.github.com>
2020-12-15 09:06:11 +0100

    Använd knappen "hämta" för att hämta position

--- public/js/script.js
+++ public/js/script.js
@@ -10,17 +10,19 @@ $(document).ready(function () {
 
 let map, infoWindow;
 
+/// JUST NU HÄMTAR "GET_DATA" PLATSTJÄNST
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 55.893180, lng: 13.582728 }, 
     zoom: 8,
   });
   infoWindow = new google.maps.InfoWindow();
-  const locationButton = document.createElement("button");
-  locationButton.textContent = "Hämta din plats";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
+  //const locationButton = document.createElement("button");
+  //locationButton.textContent = "Hämta din plats";
+  //locationButton.classList.add("custom-map-control-button");
+  //get_data.classList.add("custom-map-control-button");
+  //map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  document.querySelector('.get_data').addEventListener("click", () => {
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -29,10 +31,18 @@ function initMap() {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           };
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Plats hittad.");
+          var marker = new google.maps.Marker({
+            position: pos,
+            label: { color: '#ffffff', fontWeight: 'bold', fontSize: '8px', text: 'Start' }
+        });
+        
+          // To add the marker to the map, call setMap();
+          marker.setMap(map);
+          //infoWindow.setPosition(pos);
+          //infoWindow.setContent("Plats hittad.");
           infoWindow.open(map);
           map.setCenter(pos);
+          map.setZoom(10);
           postLatLng(pos);
         },
         () => {
