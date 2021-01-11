de5b1f8f184a3bcccb398d51ff4a17d2219a4e4f
Lull3n <61215087+Lull3n@users.noreply.github.com>
2021-01-11 13:39:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { render } = require('pug');
 const apiController = require('./APICallers/apiController');
 const app = express()
-const port = 80
+const port = 3000
 
 app.set('view engine', 'pug');
 app.set('views','./views');
